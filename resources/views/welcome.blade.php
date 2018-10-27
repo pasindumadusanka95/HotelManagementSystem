fa2c69331cfb54d3ab26bb13d870a2c8050d7205
Pasindu Madusanka <pasindusenerath@gmail.com>
2018-10-28 00:48:33 +0530

    Update resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Hotel Managemnt System</title>
 
         <!-- Fonts -->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
